ae41f602e78fba1852483a0722bc4ec4d053c5a6
chingkamhing <kam_ching@hotmail.com>
2021-07-19 00:36:19 +0800

    Tidy up deploy/cert/localhost/cert.sh

--- deploy/cert/localhost/cert.sh
+++ deploy/cert/localhost/cert.sh
@@ -10,11 +10,11 @@ DOMAIN=localhost
 OUTPUT_PATH="deploy/cert/localhost"
 
 # generate Certificate Authority (ca) first
-openssl req -x509 -new -nodes -sha256 -days 1024 -newkey rsa:2048 -keyout ${OUTPUT_PATH}/ca.key -out ${OUTPUT_PATH}/ca.pem -subj "/C=US/CN=Example-Root-CA"
-openssl x509 -outform pem -in ${OUTPUT_PATH}/ca.pem -out ${OUTPUT_PATH}/ca.crt
+openssl req -x509 -new -nodes -sha256 -newkey rsa:2048 -keyout ${OUTPUT_PATH}/ca.key -out ${OUTPUT_PATH}/ca.csr -subj "/C=HK/CN=Example-Root-CA"
+openssl x509 -outform pem -in ${OUTPUT_PATH}/ca.csr -out ${OUTPUT_PATH}/ca.pem -days 1095
 # use the ca to create server cert and private key
-openssl req -new -nodes -newkey rsa:2048 -keyout ${OUTPUT_PATH}/${DOMAIN}.key -out ${OUTPUT_PATH}/${DOMAIN}.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=localhost.local"
-openssl x509 -req -sha256 -days 1024 -in ${OUTPUT_PATH}/${DOMAIN}.csr -CA ${OUTPUT_PATH}/ca.pem -CAkey ${OUTPUT_PATH}/ca.key -CAcreateserial -extfile ${OUTPUT_PATH}/${DOMAIN}.ext -out ${OUTPUT_PATH}/${DOMAIN}.pem
+openssl req -new -nodes -sha256 -newkey rsa:2048 -keyout ${OUTPUT_PATH}/${DOMAIN}.key -out ${OUTPUT_PATH}/${DOMAIN}.csr -subj "/C=HK/ST=YourState/L=YourCity/O=Example-Certificates/CN=localhost.local"
+openssl x509 -req -sha256 -CA ${OUTPUT_PATH}/ca.pem -CAkey ${OUTPUT_PATH}/ca.key -CAcreateserial -extfile ${OUTPUT_PATH}/${DOMAIN}.ext -in ${OUTPUT_PATH}/${DOMAIN}.csr -out ${OUTPUT_PATH}/${DOMAIN}.pem -days 365
 
 # view the cert
 openssl req -in ${OUTPUT_PATH}/${DOMAIN}.csr -noout -text
