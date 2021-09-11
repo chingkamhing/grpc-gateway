c21f144753c44dc133f32996fb80b0841e69de74
chingkamhing <kam_ching@hotmail.com>
2021-09-12 04:03:09 +0800

    WIP to add authentication middleware

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 
 	tm2_proto_gateway_go "github.com/chingkamhing/grpc-gateway/lib/tm2-proto-gateway-go"
 )
@@ -24,6 +25,7 @@ func main() {
 		log.Fatalf("failed to load credentials: %v", err)
 	}
 	gatewayOptions := []grpc.DialOption{
+		grpc.WithChainUnaryInterceptor(authInterceptor),
 		// oauth.NewOauthAccess requires the configuration of transport credentials.
 		grpc.WithTransportCredentials(creds),
 	}
@@ -31,16 +33,46 @@ func main() {
 	if err != nil {
 		log.Fatalln("Failed to dial server:", err)
 	}
-	gwmux := runtime.NewServeMux()
+	gwMux := runtime.NewServeMux(
+		// hand-pick to forward http method and url path to metadata in order to be used in authentication middleware
+		runtime.WithMetadata(func(c context.Context, req *http.Request) metadata.MD {
+			return metadata.Pairs("x-forwarded-method", req.Method)
+		}),
+		runtime.WithMetadata(func(c context.Context, req *http.Request) metadata.MD {
+			return metadata.Pairs("x-forwarded-url-path", req.URL.Path)
+		}),
+	)
 	// Register Greeter
-	err = tm2_proto_gateway_go.RegisterGatewayHandler(context.Background(), gwmux, gatewayConn)
+	err = tm2_proto_gateway_go.RegisterGatewayHandler(context.Background(), gwMux, gatewayConn)
 	if err != nil {
 		log.Fatalln("Failed to register gateway:", err)
 	}
 	gwServer := &http.Server{
 		Addr:    host,
-		Handler: gwmux,
+		Handler: gwMux,
 	}
 	log.Printf("Serving http gateway on http://%s\n", host)
 	log.Fatalln(gwServer.ListenAndServe())
+}
+
+// authInterceptor authenticate endpoint access
+func authInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// get out bound metadata
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !ok {
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+	log.Printf("md: %#v", md)
+	// check if authorization is needed
+	auths, ok := md["authorization"]
+	if !ok {
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+	// need authorization
+	for _, auth := range auths {
+		//FIXME, check authorization here
+		log.Printf("auth: %#v", auth)
+	}
+	log.Printf("req: %#v", req)
+	return invoker(ctx, method, req, reply, cc, opts...)
 }
