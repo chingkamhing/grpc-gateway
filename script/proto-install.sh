a8825d7e3ca7399bd5c2d6c10fa59775a5de1520
chingkamhing <kam_ching@hotmail.com>
2021-12-03 21:40:08 +0800

    Update script to update grpc tool chain

--- script/proto-install.sh
+++ script/proto-install.sh
@@ -48,9 +48,23 @@ unzip -o $PROTOC_ZIP -d ${INSTALL_DIR} bin/protoc
 unzip -o $PROTOC_ZIP -d ${INSTALL_DIR}/bin 'include/*'
 rm -f $PROTOC_ZIP
 
-# install protocol buffer libraries
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2
-go install google.golang.org/protobuf/cmd/protoc-gen-go
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-go install github.com/favadi/protoc-go-inject-tag
+# update/download necessary proto files
+if [ -d ${GOPATH}/src/github.com/grpc-ecosystem ]; then
+	git -C ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway pull https://github.com/grpc-ecosystem/grpc-gateway.git
+else
+	mkdir -p ${GOPATH}/src/github.com/grpc-ecosystem
+	git -C ${GOPATH}/src/github.com/grpc-ecosystem clone https://github.com/grpc-ecosystem/grpc-gateway.git
+fi
+if [ -d ${GOPATH}/src/github.com/googleapis ]; then
+	git -C ${GOPATH}/src/github.com/googleapis/googleapis pull https://github.com/googleapis/googleapis.git
+else
+	mkdir -p ${GOPATH}/src/github.com/googleapis
+	git -C ${GOPATH}/src/github.com/googleapis clone https://github.com/googleapis/googleapis.git
+fi
+
+# get protocol buffer libraries
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install github.com/favadi/protoc-go-inject-tag@latest
